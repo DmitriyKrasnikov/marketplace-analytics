c1b12c846349130f7218cd80d20acfffe1c9a0e5
Dmitriy <dimankrasnikov@mail.ru>
2025-08-09 13:06:15 +0300

    init commit

---
+++ src/main/java/com/krasnikov/marketplaceanalytics/MarketplaceAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.krasnikov.marketplaceanalytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketplaceAnalyticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarketplaceAnalyticsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/krasnikov/marketplaceanalytics/MarketplaceAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krasnikov.marketplaceanalytics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceAnalyticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
